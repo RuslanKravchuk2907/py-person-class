0c8d6b3cd31216f8ad55ab96d1c92f8f601f1fe6
melad <meladzeruslan164@gmail.com>
2024-11-12 03:40:08 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,30 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
+
+    @classmethod
+    def reset_people(cls) -> None:
+        cls.people.clear()
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    Person.reset_people()
+
+    person_instances = []
+    for person_data in people:
+        # Create a new Person instance
+        person = Person(name=person_data["name"], age=person_data["age"])
+        person_instances.append(person)
+
+    for person_data in people:
+        person = Person.people[person_data["name"]]
+        if "wife" in person_data and person_data["wife"] is not None:
+            person.wife = Person.people.get(person_data["wife"])
+        elif "husband" in person_data and person_data["husband"] is not None:
+            person.husband = Person.people.get(person_data["husband"])
+
+    return person_instances
